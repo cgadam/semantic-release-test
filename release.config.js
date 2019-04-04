ffdbe3091c9259a2899b2aa9258ce843bfb147e7
Christian Adam <christian@thirdlove.com>
2019-04-04 12:31:08 -0300

    chor(semantic-versioning) Add release config

---
+++ release.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  branch: 'master',
+  repositoryUrl: 'git@github.com:cgadam/semantic-release-test.git',
+  tagFormat: 'version-{$version}',
+  ci: false,
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/github'
+  ],
+};
